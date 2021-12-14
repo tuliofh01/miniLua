edf5d97a410b4f9a0289daf33cb7f19e128c444e
tuliofh01 <tuliofh01@gmail.com>
2021-12-14 15:48:24 -0300

    Adiciona token table e token types.

--- Java/lexical/TokenType.java
+++ Java/lexical/TokenType.java
@@ -7,10 +7,54 @@ public enum TokenType {
     END_OF_FILE,
 
     // SYMBOLS
+    SEMI_COLON, // ;
+    COLON,      // ,
+    OPEN_PAR,   // (
+    CLOSE_PAR,  // )
+    DOT,        // .
+    OPEN_BRA,   // [
+    CLOSE_BRA,  // ]
+    OPEN_CUR,   // {
+    CLOSE_CUR,  // }
 
     // OPERATORS
+    ASSIGN,         // =
+    AND,            // and
+    OR,             // or
+    LOWER_THAN,     // <
+    GREATER_THAN,   // >
+    LOWER_EQUAL,    // <=
+    GREATER_EQUAL,  // >=
+    NOT_EQUAL,      // ~=
+    EQUAL,          // ==
+    CONCAT,         // ..
+    ADD,            // +
+    SUB,            // -
+    MUL,            // *
+    DIV,            // /
+    MOD,            // %
+    SIZE,           // #
+    NOT,            // not
 
     // KEYWORDS
+    IF,         // if
+    THEN,       // then
+    ELSEIF,     // elseif
+    ELSE,       // else
+    END,        // end
+    WHILE,      // while
+    DO,         // do
+    REPEAT,     // repeat
+    UNTIL,      // until
+    FOR,        // for
+    IN,         // in
+    PRINT,      // print
+    FALSE,      // false
+    TRUE,       // true
+    NIL,        // nil
+    READ,       // read
+    TONUMBER,   // tonumber
+    TOSTRING,   // tostring
 
     // OTHERS
     ID,            // identifier
