9a6759bc818c067d70b40d0909a7e15b672b833c
tuliofh01 <tuliofh01@gmail.com>
2021-12-19 18:35:45 -0300

    Sintatica

--- Java/syntatic/SyntaticAnalysis.java
+++ Java/syntatic/SyntaticAnalysis.java
@@ -4,23 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
-/*
-import interpreter.command.AssignCommand;
-import interpreter.command.BlocksCommand;
-import interpreter.command.Command;
-import interpreter.command.PrintCommand;
-import interpreter.command.WhileCommand;
-import interpreter.expr.ConstExpr;
-import interpreter.expr.Expr;
-import interpreter.expr.SetExpr;
-import interpreter.expr.UnaryOp;
-import interpreter.expr.UnaryExpr;
-import interpreter.expr.Variable;
-import interpreter.value.BooleanValue;
-import interpreter.value.NumberValue;
-import interpreter.value.StringValue;
-import interpreter.value.Value;
-*/
+
 import lexical.Lexeme;
 import lexical.LexicalAnalysis;
 import lexical.TokenType;
@@ -77,324 +61,229 @@ public class SyntaticAnalysis {
 
         System.exit(1);
     }
-    /*
-     * // <code> ::= { <cmd> }
-     * private BlocksCommand procCode() {
-     * int line = lex.getLine();
-     * List<Command> cmds = new ArrayList<Command>();
-     * while (current.type == TokenType.IF ||
-     * current.type == TokenType.WHILE ||
-     * current.type == TokenType.REPEAT ||
-     * current.type == TokenType.FOR ||
-     * current.type == TokenType.PRINT ||
-     * current.type == TokenType.ID) {
-     * Command cmd = procCmd();
-     * cmds.add(cmd);
-     * }
-     * 
-     * BlocksCommand bc = new BlocksCommand(line, cmds);
-     * return bc;
-     * }
-     * 
-     * // <cmd> ::= (<if> | <while> | <repeat> | <for> | <print> | <assign>) [';']
-     * private Command procCmd() {
-     * Command cmd = null;
-     * if (current.type == TokenType.IF) {
-     * procIf();
-     * } else if (current.type == TokenType.WHILE) {
-     * cmd = procWhile();
-     * } else if (current.type == TokenType.REPEAT) {
-     * procRepeat();
-     * } else if (current.type == TokenType.FOR) {
-     * procFor();
-     * } else if (current.type == TokenType.PRINT) {
-     * cmd = procPrint();
-     * } else if (current.type == TokenType.ID) {
-     * cmd = procAssign();
-     * } else {
-     * showError();
-     * }
-     * 
-     * if (current.type == TokenType.SEMI_COLON)
-     * advance();
-     * 
-     * return cmd;
-     * }
-     * 
-     * // <if> ::= if <expr> then <code> { elseif <expr> then <code> } [ else <code>
-     * ]
-     * // end
-     * private void procIf() {
-     * eat(TokenType.IF);
-     * procExpr();
-     * eat(TokenType.THEN);
-     * procCode();
-     * while (current.type == TokenType.ELSEIF) {
-     * advance();
-     * procExpr();
-     * eat(TokenType.THEN);
-     * procCode();
-     * }
-     * if (current.type == TokenType.ELSE) {
-     * advance();
-     * procCode();
-     * }
-     * eat(TokenType.END);
-     * }
-     * 
-     * // <while> ::= while <expr> do <code> end
-     * private WhileCommand procWhile() {
-     * eat(TokenType.WHILE);
-     * int line = lex.getLine();
-     * 
-     * Expr expr = procExpr();
-     * eat(TokenType.DO);
-     * Command cmd = procCode();
-     * eat(TokenType.END);
-     * 
-     * WhileCommand wc = new WhileCommand(line, expr, cmd);
-     * return wc;
-     * }
-     * 
-     * // <repeat> ::= repeat <code> until <expr>
-     * private void procRepeat() {
-     * }
-     * 
-     * // <for> ::= for <name> (('=' <expr> ',' <expr> [',' <expr>]) | ([',' <name>]
-     * in
-     * // <expr>)) do <code> end
-     * private void procFor() {
-     * }
-     * 
-     * // <print> ::= print '(' [ <expr> ] ')'
-     * private PrintCommand procPrint() {
-     * eat(TokenType.PRINT);
-     * int line = lex.getLine();
-     * eat(TokenType.OPEN_PAR);
-     * 
-     * Expr expr = null;
-     * if (current.type == TokenType.OPEN_PAR ||
-     * current.type == TokenType.SUB ||
-     * current.type == TokenType.SIZE ||
-     * current.type == TokenType.NOT ||
-     * current.type == TokenType.NUMBER ||
-     * current.type == TokenType.STRING ||
-     * current.type == TokenType.FALSE ||
-     * current.type == TokenType.TRUE ||
-     * current.type == TokenType.NIL ||
-     * current.type == TokenType.READ ||
-     * current.type == TokenType.TONUMBER ||
-     * current.type == TokenType.TOSTRING ||
-     * current.type == TokenType.OPEN_CUR ||
-     * current.type == TokenType.ID) {
-     * expr = procExpr();
-     * }
-     * 
-     * eat(TokenType.CLOSE_PAR);
-     * 
-     * PrintCommand pc = new PrintCommand(line, expr);
-     * return pc;
-     * }
-     * 
-     * // <assign> ::= <lvalue> { ',' <lvalue> } '=' <expr> { ',' <expr> }
-     * private AssignCommand procAssign() {
-     * Vector<SetExpr> lhs = new Vector<SetExpr>();
-     * Vector<Expr> rhs = new Vector<Expr>();
-     * 
-     * lhs.add(procLValue());
-     * while (current.type == TokenType.COLON) {
-     * advance();
-     * lhs.add(procLValue());
-     * }
-     * eat(TokenType.ASSIGN);
-     * int line = lex.getLine();
-     * 
-     * rhs.add(procExpr());
-     * while (current.type == TokenType.COLON) {
-     * advance();
-     * rhs.add(procExpr());
-     * }
-     * 
-     * AssignCommand ac = new AssignCommand(line, lhs, rhs);
-     * return ac;
-     * }
-     * 
-     * // <expr> ::= <rel> { (and | or) <rel> }
-     * private Expr procExpr() {
-     * Expr expr = procRel();
-     * while (current.type == TokenType.AND || current.type == TokenType.OR) {
-     * advance();
-     * // FIXME: Implement me!
-     * procRel();
-     * }
-     * 
-     * return expr;
-     * }
-     * 
-     * // <rel> ::= <concat> [ ('<' | '>' | '<=' | '>=' | '~=' | '==') <concat> ]
-     * private Expr procRel() {
-     * Expr expr = procConcat();
-     * // FIXME: Implement me!
-     * 
-     * return expr;
-     * }
-     * 
-     * // <concat> ::= <arith> { '..' <arith> }
-     * private Expr procConcat() {
-     * Expr expr = procArith();
-     * // FIXME: Implement me!
-     * 
-     * return expr;
-     * }
-     * 
-     * // <arith> ::= <term> { ('+' | '-') <term> }
-     * private Expr procArith() {
-     * Expr expr = procTerm();
-     * // FIXME: Implement me!
-     * 
-     * return expr;
-     * }
-     * 
-     * // <term> ::= <factor> { ('*' | '/' | '%') <factor> }
-     * private Expr procTerm() {
-     * Expr expr = procFactor();
-     * // FIXME: Implement me!
-     * 
-     * return expr;
-     * }
-     * 
-     * // <factor> ::= '(' <expr> ')' | [ '-' | '#' | not ] <rvalue>
-     * private Expr procFactor() {
-     * Expr expr = null;
-     * if (current.type == TokenType.OPEN_PAR) {
-     * // FIXME: Implement me!
-     * } else {
-     * UnaryOp op = null;
-     * if (current.type == TokenType.SUB) {
-     * advance();
-     * op = UnaryOp.Neg;
-     * } else if (current.type == TokenType.SIZE) {
-     * advance();
-     * op = UnaryOp.Size;
-     * } else if (current.type == TokenType.NOT) {
-     * advance();
-     * op = UnaryOp.Not;
-     * }
-     * int line = lex.getLine();
-     * 
-     * expr = procRValue();
-     * 
-     * if (op != null)
-     * expr = new UnaryExpr(line, expr, op);
-     * }
-     * 
-     * return expr;
-     * }
-     * 
-     * // <lvalue> ::= <name> { '.' <name> | '[' <expr> ']' }
-     * private SetExpr procLValue() {
-     * Variable var = procName();
-     * 
-     * while (current.type == TokenType.DOT ||
-     * current.type == TokenType.OPEN_BRA) {
-     * if (current.type == TokenType.DOT) {
-     * advance();
-     * procName();
-     * } else {
-     * advance();
-     * procExpr();
-     * eat(TokenType.CLOSE_BRA);
-     * }
-     * }
-     * 
-     * return var;
-     * }
-     * 
-     * // <rvalue> ::= <const> | <function> | <table> | <lvalue>
-     * private Expr procRValue() {
-     * Expr expr = null;
-     * if (current.type == TokenType.NUMBER ||
-     * current.type == TokenType.STRING ||
-     * current.type == TokenType.FALSE ||
-     * current.type == TokenType.TRUE ||
-     * current.type == TokenType.NIL) {
-     * Value<?> v = procConst();
-     * int line = lex.getLine();
-     * expr = new ConstExpr(line, v);
-     * } else if (current.type == TokenType.READ ||
-     * current.type == TokenType.TONUMBER ||
-     * current.type == TokenType.TOSTRING) {
-     * procFunction();
-     * } else if (current.type == TokenType.OPEN_CUR) {
-     * procTable();
-     * } else if (current.type == TokenType.ID) {
-     * expr = procLValue();
-     * } else {
-     * showError();
-     * }
-     * 
-     * return expr;
-     * }
-     * 
-     * // <const> ::= <number> | <string> | false | true | nil
-     * private Value<?> procConst() {
-     * Value<?> v = null;
-     * if (current.type == TokenType.NUMBER) {
-     * v = procNumber();
-     * } else if (current.type == TokenType.STRING) {
-     * v = procString();
-     * } else if (current.type == TokenType.FALSE) {
-     * advance();
-     * v = new BooleanValue(false);
-     * } else if (current.type == TokenType.TRUE) {
-     * advance();
-     * v = new BooleanValue(true);
-     * } else if (current.type == TokenType.NIL) {
-     * advance();
-     * v = null;
-     * } else {
-     * showError();
-     * }
-     * 
-     * return v;
-     * }
-     * 
-     * // <function> ::= (read | tonumber | tostring) '(' [ <expr> ] ')'
-     * private void procFunction() {
-     * }
-     * 
-     * // <table> ::= '{' [ <elem> { ',' <elem> } ] '}'
-     * private void procTable() {
-     * }
-     * 
-     * // <elem> ::= [ '[' <expr> ']' '=' ] <expr>
-     * private void procElem() {
-     * }
-     * 
-     * private Variable procName() {
-     * String name = current.token;
-     * eat(TokenType.ID);
-     * int line = lex.getLine();
-     * 
-     * Variable var = new Variable(line, name);
-     * return var;
-     * }
-     * 
-     * private NumberValue procNumber() {
-     * String tmp = current.token;
-     * eat(TokenType.NUMBER);
-     * 
-     * Double d = Double.valueOf(tmp);
-     * NumberValue nv = new NumberValue(d);
-     * return nv;
-     * }
-     * 
-     * private StringValue procString() {
-     * String tmp = current.token;
-     * eat(TokenType.STRING);
-     * 
-     * StringValue sv = new StringValue(tmp);
-     * return sv;
-     * }
-     */
+    
+     // <code> ::= { <cmd> }
+    private void procCode() {
+        while(current.type == TokenType.IF || current.type == TokenType.WHILE || current.type == TokenType.REPEAT || current.type == TokenType.FOR || current.type == TokenType.PRINT || current.type == TokenType.ID){
+            procCmd();
+        }
+     }
+      
+     // <cmd> ::= (<if> | <while> | <repeat> | <for> | <print> | <assign>) [';']
+    private void procCmd() {
+        if (current.type == TokenType.IF){
+            procIf();
+        }
+        else if(current.type == TokenType.WHILE){
+            procWhile();
+        }
+        else if(current.type == TokenType.REPEAT){
+            procRepeat();
+        }
+        else if(current.type == TokenType.FOR){
+            procFor();
+        }
+        else if(current.type == TokenType.PRINT){
+            procPrint();
+        }
+        else if(current.type == TokenType.ID){
+            procAssign();
+        }
+        else{
+            showError();
+        }
+        if (current.type == TokenType.COLON){
+            eat(TokenType.SEMI_COLON);
+        }
+     }
+        
+     // <if> ::= if <expr> then <code> { elseif <expr> then <code> } [ else <code> ] end
+    private void procIf() {
+        eat(TokenType.IF);
+        procExpr();
+        eat(TokenType.THEN);
+        procCode();
+        while (current.type == TokenType.ELSEIF) {
+            advance();
+            procExpr();
+            eat(TokenType.THEN);
+            procCode();
+        }
+        if(current.type == TokenType.ELSE){
+            procCode();
+        }
+        eat(TokenType.END);
+     }
+      
+     // <while> ::= while <expr> do <code> end
+    private void procWhile() {
+        eat(TokenType.WHILE);
+        procExpr();
+        eat(TokenType.DO);
+        procCode();
+        eat(TokenType.END);
+     }
+      
+     // <repeat> ::= repeat <code> until <expr>
+    private void procRepeat() {
+        eat(TokenType.REPEAT);
+        procCode();
+        eat(TokenType.UNTIL);
+        procExpr();
+     }
+      
+     // <for> ::= for <name> (('=' <expr> ',' <expr> [',' <expr>]) | ([',' <name>]
+    private void procFor() {
+        eat(TokenType.FOR);
+        procName();
+        
+        if (current.type == TokenType.ASSIGN){
+
+        }
+        else if ( current.type == TokenType.COLON){
+
+        }
+
+     }
+      
+     // <print> ::= print '(' [ <expr> ] ')'
+    private void procPrint() {
+        eat(TokenType.PRINT);
+        eat(TokenType.OPEN_PAR);
+        procExpr();
+        eat(TokenType.CLOSE_PAR);
+     }
+      
+     // <assign> ::= <lvalue> { ',' <lvalue> } '=' <expr> { ',' <expr> }
+    private void procAssign() {
+        procLValue();
+        while (current.type == TokenType.COLON) {
+            advance();
+            procLValue();
+        }
+        eat(TokenType.ASSIGN);
+        procExpr();
+        while (current.type == TokenType.COLON) {
+            advance();
+            procExpr();
+        }
+     }
+     
+     // <expr> ::= <rel> { (and | or) <rel> }
+    private void procExpr() {
+        procRel();
+        while(current.type == TokenType.AND || current.type == TokenType.OR){
+            advance();
+            procRel();
+        }
+     } 
+     
+     // <rel> ::= <concat> [ ('<' | '>' | '<=' | '>=' | '~=' | '==') <concat> ]
+    private void procRel() {
+        procConcat();
+        if(current.type == TokenType.LOWER_THAN || current.type == TokenType.GREATER_THAN || current.type == TokenType.LOWER_EQUAL || current.type == TokenType.GREATER_EQUAL || current.type == TokenType.NOT_EQUAL || current.type == TokenType.EQUAL){
+            advance();
+            procConcat();
+        }
+     }
+     
+     // <concat> ::= <arith> { '..' <arith> }
+    private void procConcat() {
+        procArith();
+        while (current.type == TokenType.CONCAT){
+            advance();
+            procArith();
+        }
+    }
+     
+     // <arith> ::= <term> { ('+' | '-') <term> }
+    private void procArith() {
+     }
+      
+      // <term> ::= <factor> { ('*' | '/' | '%') <factor> }
+    private void procTerm() {
+     }
+      
+     // <factor> ::= '(' <expr> ')' | [ '-' | '#' | not ] <rvalue>
+    private void procFactor() {
+     }
+     
+     // <lvalue> ::= <name> { '.' <name> | '[' <expr> ']' }
+    private void procLValue() {
+        procName();
+        while (current.type == TokenType.DOT || current.type == TokenType.OPEN_BRA){
+            if (current.type == TokenType.DOT){
+                advance();
+                procName();
+            }
+            else{
+                advance();
+                procExpr();
+                eat(TokenType.CLOSE_BRA);
+            }
+        }
+     }
+     
+     // <rvalue> ::= <const> | <function> | <table> | <lvalue>
+    private void procRValue() {
+        if (current.type == TokenType.READ || current.type == TokenType.TONUMBER || current.type == TokenType.TOSTRING){
+            procFunction();
+        }
+        else if(current.type == TokenType.OPEN_CUR){
+            procTable();
+        }
+        else if(current.type == TokenType.NUMBER || current.type == TokenType.STRING || current.type == TokenType.FALSE || current.type == TokenType.TRUE || current.type == TokenType.NIL){
+            procConst();
+        }
+        else {
+            procLValue();
+        }
+     }
+     
+     // <const> ::= <number> | <string> | false | true | nil
+    private void procConst() {
+        if(current.type == TokenType.NUMBER){
+            procNumber();
+        }
+        else if(current.type == TokenType.STRING){
+            procString();
+        }
+        else if(current.type == TokenType.FALSE){
+            advance();
+        }
+        else if(current.type == TokenType.TRUE){
+            advance();
+        }
+        else if(current.type == TokenType.NIL){
+            advance();
+        }
+        else {
+            showError();
+        }
+     }
+      
+    // <function> ::= (read | tonumber | tostring) '(' [ <expr> ] ')'
+    private void procFunction() {
+    }
+     
+    // <table> ::= '{' [ <elem> { ',' <elem> } ] '}'
+    private void procTable() {
+    }
+     
+    // <elem> ::= [ '[' <expr> ']' '=' ] <expr>
+    private void procElem() {
+    }
+     
+    private void procName() {
+        eat(TokenType.ID);
+    }
+     
+    private void procNumber() {
+        eat(TokenType.NUMBER);
+    }
+     
+    private void procString() {
+        eat(TokenType.STRING);
+    }
+
 }
